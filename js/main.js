eb279ac62c027dd35e1a537bfa122cfed8895000
Ben <bencoullie@gmail.com>
2014-04-15 06:40:00 -0800

    trying to fix navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+$(function() {
+		var headerheight = $(window).height()*60/100;
+		console.log("The header height is: " + headerheight);
 
+		var scrollfromtop = $(document).scroll(function() {
+    		console.log($(document).scrollTop());
+		});
+		console.log("The scroll from top is: " + scrollfromtop);
+
+
+});
+
+
+
+// $(function() {
+// 	var win         = $(window),
+// 	fixed_element   = $('nav'),
+// 	element_offset  = fixed_element.offset().top;
+
+// 	win.scroll(function() {
+// 	    if (element_offset < win.scrollTop()) {
+// 	        fixed_element.addClass("fixed");
+// 	    } else {
+// 	        fixed_element.removeClass("fixed");
+// 	    }
+// 	});
+// });
